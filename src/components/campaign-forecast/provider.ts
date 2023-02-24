d9aae374224557c9be2f5d1bef9f170414df279c
jed <johncd@codev.com>
2023-02-25 02:02:41 +0800

    limit results to football, band, baseball, basketball and robotics

--- src/components/campaign-forecast/provider.ts
+++ src/components/campaign-forecast/provider.ts
@@ -28,6 +28,7 @@ export default class CampaignForecast {
       LEFT JOIN fundraiser_addresses ON fundraiser_addresses.fundraiser_id  = fundraisers.id
       LEFT JOIN addresses ON addresses.id = fundraiser_addresses.address_id 
       WHERE fundraisers.created_at  between "2021-01-01" and "2022-12-31" and fundraisers.net_raised_cents > 0 
+      and activity in ("football", "band", "baseball", "basketball", "robotics") 
       group by year(fundraisers.created_at), month(fundraisers.created_at), addresses.zip, groups.activity;`
     );
 
